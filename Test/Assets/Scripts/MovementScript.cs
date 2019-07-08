30e6bdecad26d9fc97949e95678a4c45d0096cbd
Daniel <daniel.presas@uoit.net>
2019-07-07 23:26:15 -0400

    [ADD] Camera, player movement basics

---
+++ Test/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+    public float speed;
+
+    Rigidbody rb;
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        movement = new Vector3(0.0f, 0.0f, 0.0f);
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+
+        movement.Set(movementX, 0.0f, movementZ);
+        movement *= speed * Time.deltaTime;
+
+        rb.transform.Translate(movement);
+
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
