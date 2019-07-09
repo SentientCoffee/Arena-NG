fa0553b0569b302a0c8de5e1bd796f4ca45e039d
Daniel <daniel.presas@uoit.net>
2019-07-08 22:52:07 -0400

    [ADD] Jump script

--- Test/Assets/Scripts/MovementScript.cs
+++ Test/Assets/Scripts/MovementScript.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour {
+public class MovementScript : MonoBehaviour
+{
     public float speed;
+    public float jumpHeight;
 
     Rigidbody rb;
     Vector3 movement;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,16 +21,33 @@ public class MovementScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        float movementX = Input.GetAxis("Horizontal");
-        float movementZ = Input.GetAxis("Vertical");
-
-        movement.Set(movementX, 0.0f, movementZ);
+        // Movement with WASD and/or arrow keys
+        movement.Set(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         movement *= speed * Time.deltaTime;
-
         rb.transform.Translate(movement);
 
-        if(Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetButtonDown("Jump")) {
+            if (isGrounded) {
+                rb.AddForce(Vector3.up * jumpHeight);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             Cursor.lockState = CursorLockMode.None;
         }
+
+        isGrounded = false;
     }
+
+    void OnCollisionStay(Collision other) {
+        if(other.gameObject.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
+
+    //void OnCollisionExit(Collision other) {
+    //    if (other.gameObject.tag == "Ground") {
+    //        isGrounded = false;
+    //    }
+    //}
 }
